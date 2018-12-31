4a0b998c831fd14522640b573bdf6ac9623b1c28
Emil Poulsen <emil-poulsen@hotmail.com>
2018-12-31 05:02:22 +0100

    reading props from gh components works

--- Ducker/Program.cs
+++ Ducker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,47 @@ namespace Ducker
 {
     class Program
     {
+        static bool initialized = false;
+        static string systemDir = null;
+
+        public static void AssemblyInitialize()
+        {
+            if (initialized)
+            {
+                throw new InvalidOperationException("AssemblyInitialize should only be called once");
+            }
+            initialized = true;
+
+            // Ensure we are 64 bit
+            if (!Environment.Is64BitProcess) {
+                throw new Exception("Tests must be run as x64");
+            } 
+
+            // Set path to rhino system directory
+            string envPath = Environment.GetEnvironmentVariable("path");
+             string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            systemDir = System.IO.Path.Combine(programFiles, "Rhino WIP", "System");
+
+            if (!System.IO.Directory.Exists(systemDir))
+            {
+                throw new Exception(string.Format("Rhino system dir not found: {0}", systemDir));
+            }
+            
+            // Add rhino system directory to path (for RhinoLibrary.dll)
+            Environment.SetEnvironmentVariable("path", envPath + ";" + systemDir);
+
+            // Add hook for .Net assmbly resolve (for RhinoCommmon.dll)
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            // Start headless Rhino process
+            LaunchInProcess(0, 0);
+
+        }
+
         static void Main(string[] args)
         {
+            AssemblyInitialize();
+
             string pathToDll = @"C:\Users\epoulsen\Documents\GitHub\Emu\Emu\Emu.Grasshopper\bin\Release\Emu.Grasshopper.gha";
             var DLL = Assembly.LoadFile(pathToDll);
             string folder = Path.GetDirectoryName(pathToDll) + @"\";
@@ -20,6 +60,8 @@ namespace Ducker
             List<Assembly> dependencies = new List<Assembly>();
 
             AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+
 
             for (int i = 0; i < asm.Length; i++)
             {
@@ -36,14 +78,58 @@ namespace Ducker
 
             foreach (Type type in DLL.GetExportedTypes())
             {
+
+                if (IsDerivedFromGhComponent(type))
+                {
+                    dynamic c = Activator.CreateInstance(type);
+                    string s = c.Description;
+
+                    
+                }
+
+                
+                //
+                //type.IsAssignableFrom(typeof(GH_Component))
                 //var c = Activator.CreateInstance(type);
                 //type.InvokeMember("Output", BindingFlags.InvokeMethod, null, c, new object[] { @"Hello" });
             }
 
         }
 
+        private static bool IsDerivedFromGhComponent(Type type)
+        {
+            Type currType = type;
+
+            while(currType != null)
+            {
+                if (currType.Name.StartsWith("GH_Component"))
+                {
+                    return true;
+                }
+                currType = currType.BaseType;
+            }
+            return false;
+        }
+
+        [DllImport("RhinoLibrary.dll")]
+        internal static extern int LaunchInProcess(int reserved1, int reserved2);
+
+
         private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
+            var name = args.Name;
+
+            if (name.StartsWith("RhinoCommon"))
+            {
+                var path = System.IO.Path.Combine(systemDir, "RhinoCommon.dll");
+                return Assembly.LoadFrom(path);
+            }
+
+            if (name.StartsWith("Grasshopper"))
+            {
+
+            }
+
             // Ignore missing resources
             if (args.Name.Contains(".resources"))
                 return null;
@@ -67,7 +153,7 @@ namespace Ducker
             {
                 return null;
             }
-            
+
         }
     }
 }
