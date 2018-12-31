184c99a4002f6cc624fb4c66875ad7631e40f19f
Emil Poulsen <emil-poulsen@hotmail.com>
2018-12-31 04:20:07 +0100

    Revert "failed experiment"
    This reverts commit 6d257fee505edd469304714ef9bd56af6d949a48.

--- Ducker/Program.cs
+++ Ducker/Program.cs
@@ -5,77 +5,40 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using Grasshopper;
-using Grasshopper.Kernel;
 
 namespace Ducker
 {
-    public class TestComponent : GH_Component
-    {
-        public override Guid ComponentGuid
-        {
-            get
-            {
-                return Guid.NewGuid();
-            }
-        }
-
-        protected override void RegisterInputParams(GH_InputParamManager pManager)
-        {
-
-        }
-
-        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
-        {
-
-        }
-
-        protected override void SolveInstance(IGH_DataAccess DA)
-        {
-    
-        }
-    }
-
     class Program
     {
-
-
         static void Main(string[] args)
         {
+            string pathToDll = @"C:\Users\epoulsen\Documents\GitHub\Emu\Emu\Emu.Grasshopper\bin\Release\Emu.Grasshopper.gha";
+            var DLL = Assembly.LoadFile(pathToDll);
+            string folder = Path.GetDirectoryName(pathToDll) + @"\";
 
-            TestComponent component = new TestComponent();
+            AssemblyName[] asm = DLL.GetReferencedAssemblies();
+            List<Assembly> dependencies = new List<Assembly>();
 
-            //string pathToDll = @"C:\Users\epoulsen\Documents\GitHub\Emu\Emu\Emu.Grasshopper\bin\Release\Emu.Grasshopper.gha";
-            //var DLL = Assembly.LoadFile(pathToDll);
-            //string folder = Path.GetDirectoryName(pathToDll) + @"\";
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
 
-            //AssemblyName[] asm = DLL.GetReferencedAssemblies();
-            //List<Assembly> dependencies = new List<Assembly>();
+            for (int i = 0; i < asm.Length; i++)
+            {
+                string path = folder + asm[i].Name + ".dll";
+                if (File.Exists(path))
+                {
+                    Assembly dependency = Assembly.LoadFile(path);
+                    dependencies.Add(dependency);
+                }
+                //paths.Add(path);
 
-            //TestComponent component = new TestComponent();
-
-
-            //AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
-
-            //for (int i = 0; i < asm.Length; i++)
-            //{
-            //    string path = folder + asm[i].Name + ".dll";
-            //    if (File.Exists(path))
-            //    {
-            //        Assembly dependency = Assembly.LoadFile(path);
-            //        dependencies.Add(dependency);
-            //    }
-            //    //paths.Add(path);
-
-            //}
+            }
 
 
-
-            //foreach (Type type in DLL.GetExportedTypes())
-            //{
-            //    //var c = Activator.CreateInstance(type);
-            //    //type.InvokeMember("Output", BindingFlags.InvokeMethod, null, c, new object[] { @"Hello" });
-            //}
+            foreach (Type type in DLL.GetExportedTypes())
+            {
+                //var c = Activator.CreateInstance(type);
+                //type.InvokeMember("Output", BindingFlags.InvokeMethod, null, c, new object[] { @"Hello" });
+            }
 
         }
 
@@ -85,9 +48,8 @@ namespace Ducker
             if (args.Name.Contains(".resources"))
                 return null;
 
-            Assembly[] loaded = AppDomain.CurrentDomain.GetAssemblies();
             // check for assemblies already loaded
-            Assembly assembly = loaded.FirstOrDefault(a => a.FullName == args.Name);
+            Assembly assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.FullName == args.Name);
             if (assembly != null)
                 return assembly;
 
