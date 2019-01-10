49ce134a591618c0f531d4c105dfe893024fd92d
Emil Poulsen <emil-poulsen@hotmail.com>
2019-01-10 01:47:44 +0100

    added more components for UI

--- Ducker.UI/MainWindow.xaml.cs
+++ Ducker.UI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,30 @@ namespace Ducker.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int InsertBindingHere { get; set; } = 50;
+
         public MainWindow()
         {
             InitializeComponent();
+            double scale = 0.3;
+            this.Width = IPhoneXWidth(scale);
+            this.Height = IPhoneXHeigh(scale);
+            string assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            this.Title = string.Format("Ducker {0}", assemblyVersion);
+            cmbColors.ItemsSource = typeof(Colors).GetProperties();
+            this.pbStatus.Value = 50;
+        }
+
+        private int IPhoneXWidth(double scale)
+        {
+            double width =  1125 * scale;
+            return (int) width;
+        }
+
+        private int IPhoneXHeigh(double scale)
+        {
+            double height = 2436 * scale;
+            return (int)height;
         }
 
         private void btnSetPath_Click(object sender, RoutedEventArgs e)
