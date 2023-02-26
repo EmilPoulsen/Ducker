3ea1df45cef11a0a2d0377f9b3c4bb29b5a4a01f
Emil Poulsen <emil-poulsen@hotmail.com>
2023-02-26 18:42:32 +0100

    comment about the console app

--- Ducker.Console/Program.cs
+++ Ducker.Console/Program.cs
@@ -15,7 +15,12 @@ namespace Ducker
 
         static void Main(string[] args)
         {
-            string pathToDll = @"C:\Users\epoulsen\Documents\GitHub\Emu\Emu\Emu.Grasshopper\bin\Release\Emu.Grasshopper.gha";
+            //TODO. https://github.com/EmilPoulsen/Ducker/issues/1 
+            // The idea here was to create a simple console app, instead of the
+            // full blown WPF app, so that Ducker can be included in automation workflows.
+            // It never got there, but please feel free to give it a go if you read this :)
+
+            string pathToDll = @"C:\Users\emil\Documents\GitHub\Emu\Emu\Emu.Grasshopper\bin\Release\Emu.Grasshopper.gha";
             
             IGhaReader reader = new RhinoHeadlessGhaReader();
             IDocGenerator docGen = new EmuMdDocGenerator();
@@ -23,9 +28,6 @@ namespace Ducker
 
             DuckRunner ducker = new DuckRunner(pathToDll);
             ducker.Run(reader, docGen, docWrite);
-
-
-
         }
     }
 }
